0e504be2ebcc35865b71cf872bc4d68d3a6b2628
ramneetk <33604200+ramneetk@users.noreply.github.com>
2021-02-22 15:29:36 -0500

    Add files via upload

--- classification/check_OOD.py
+++ classification/check_OOD.py
@@ -216,6 +216,7 @@ def calc_p_values(n, test_cel, cal_set_cel, is_val_set):
 
     compare = test_cel_reshaped<=cal_set_cel_reshaped
     p_values = np.sum(compare, axis=1)
+    p_values = p_values/len(cal_set_cel)
     
     if is_val_set==1:
         np.savez("cifar10_class{}_p_values_n{}.npz".format(opt.indist_class, n), p_values=p_values)
